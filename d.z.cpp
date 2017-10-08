0ddf859af871c9d2edeac43eb69ac78c10eda207
KristinaStr <32191518+KristinaStr@users.noreply.github.com>
2017-10-08 03:33:49 -0700

    Update d.z.cpp

--- d.z.cpp
+++ d.z.cpp
@@ -6,17 +6,27 @@ using namespace std;
 
 int main()
 {
-    int a[10];
-    int sum=0;
-    for (int i=0;i<10;i++)
+int a[10];
+int sum=0;
+int max=0;
+for (int i=0;i<10;i++)
+{
+    cin >> a[i];
+    if (a[i]>0) 
     {
-        cin >> a[i];
-        if (a[i]>0)
-            sum=sum+a[i];
+        sum=sum+a[i];
     }
-    cout<< sum;
-    return 0 ;
-    cin.get();
- 
- 
+    else if(sum>max)
+    {
+			      max=sum;
+        sum=0;
+			
+    }
+}
+    if (max==0 && sum!=0)
+    {
+        max=sum ;
+    }
+cout<< max;
+return 0 ;
 }
